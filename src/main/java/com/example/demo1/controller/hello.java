4f12ddf35d8e6b2c74cc7e088c89e3a0b111bf9e
wooziLL <2943508489@qq.com>
2021-10-12 23:34:21 +0800

    demo1

--- src/main/java/com/example/demo1/controller/hello.java
+++ src/main/java/com/example/demo1/controller/hello.java
@@ -8,6 +8,7 @@ public class hello {
     @RequestMapping("/hello")
     public String TestController(){
         String hi="helloword Spring Boot！这是一个用Spring Boot开发的网站。";
+        int s=17;
         return hi;
     }
 }
