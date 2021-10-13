e79413cbb5e30668a2646547f440b40dfe6881d1
wooziLL <2943508489@qq.com>
2021-10-13 16:07:33 +0800

    Revert "demo1"
    This reverts commit 2b84a06a

---
+++ src/main/java/com/example/demo1/controller/hello.java
@@ -0,0 +1,14 @@
+package com.example.demo1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @RequestMapping("/hello")
+    public String TestController(){
+        String hi="helloword Spring Boot！这是一个用Spring Boot开发的网站。";
+        int s=17;
+        return hi;
+    }
+}
